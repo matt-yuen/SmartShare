cba8c8429a7e63ee98ce28f3a776fcafa3986705
Matthew Yuen <un.matthew@gmail.com>
2019-01-19 16:08:02 -0500

    Created basic layout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,47 @@ import React, { Component } from "react";
 
 import Header from "./components/Header";
 import CarList from "./components/CarList";
-import Map from "./components/Map";
+import MapContainer from "./components/MapContainer";
+
+const navbarFlex = {
+  display: "flex"
+};
+
+const carListContainer = {
+  "flex-grow": 1,
+  position: "relative",
+  top: "25px",
+  left: "25px",
+  width: "300px"
+};
+
+const mapContainer = {
+  "flex-grow": 2,
+  position: "relative",
+  top: "25px",
+  right: "25px"
+};
+
+const mapSize = {
+  width: "1240px",
+  height: "550px"
+};
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <CarList />
-        <Map />
+        <div style={navbarFlex}>
+          <div style={carListContainer}>
+            <CarList />
+          </div>
+          <div style={mapContainer}>
+            <div style={mapSize}>
+              <MapContainer />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/MapContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={15}
+        initialCenter={{
+          lat: 43.6596426,
+          lng: -79.3998563
+        }}
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyA3X-16HuXy6I4ezW7mPCsAdkv2xuEvkTo"
+})(MapContainer);
+
+// export default MapContainer;
